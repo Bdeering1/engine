60895853433c1a42b945c7d315df444285a8a417
Joshua-Deering <jidjosh2006@icloud.com>
2024-01-08 23:15:34 -0500

    changed concurrency and added output file

--- tools/src/cutechess.rs
+++ tools/src/cutechess.rs
@@ -7,25 +7,27 @@ pub fn run(args: &Vec<String>) {
 
    println!("running cutechess from {}", cutechess);
 
-   //args should be engine1, engine 2, # of rounds
-
-   let arg0 = format!("conf={}",args[0]);
-   let arg1 = format!("conf={}",args[1]);
-   let arg2 = format!("{}",args[2]);
+   //args should be engine1, engine 2, # of rounds, output file name
+   let engine1 = format!("conf={}",args[0]);
+   let engine2 = format!("conf={}",args[1]);
+   let rounds = format!("{}",args[2]);
+   //need to find a way to send get the absolute path of this OS agnostic
+   //let output_file = format!("{}{}.pgn", somepath, args[3]);
 
    let run_args = vec!(
       "-engine",
-      arg0.as_str(),
+      engine1.as_str(),
       "-engine",
-      arg1.as_str(),
+      engine2.as_str(),
       "-each","tc=0/60+0",
       "-maxmoves","1000",
+      /*"-pgnout", output_file.as_str(),*/
       "-games","2",
       "-repeat", "-recover",
       "-resultformat", "wide2",
       "-ratinginterval", "10",
-      "-rounds", arg2.as_str(),
-      "-concurrency", "2",
+      "-rounds", rounds.as_str(),
+      "-concurrency", "4",
       "-tournament", "gauntlet",
       "-openings", "file=C:\\Users\\Josh\\Desktop\\Code Stuff\\rust\\engine\\tools\\res\\openings1.epd", "format=epd", "order=random"
    );
@@ -34,5 +36,5 @@ pub fn run(args: &Vec<String>) {
    
    //this makes sure that the child process actually exits when the rest of the program exits
    cutechess_program.wait().expect("Cutechess failed to execute");
-   
+
 }
