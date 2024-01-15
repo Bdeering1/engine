aca9d2f5289b3e77f1da5dd02379c93b34829ca6
Bryn <bdeering@uoguelph.ca>
2024-01-15 15:17:39 -0500

    Naive tt implementation

--- engine/src/tt.rs
+++ engine/src/tt.rs
@@ -1,14 +1,19 @@
+use std::mem::size_of;
+
 use crate::{board::Move, eval::Eval};
 
 #[repr(u8)]
+#[derive(Clone, Copy, Default)]
 pub enum Bound {
+    #[default]
     Upper,
     Lower,
     Exact,
 }
 
+#[derive(Clone, Copy, Default)] 
 struct Transposition {
-    pub hash: u64,
+    pub key: u64,
     pub score: Eval,
     pub depth: u8,
     pub bound: Bound,
@@ -21,26 +26,42 @@ struct TranspositionTable {
 
 impl Default for TranspositionTable {
     fn default() -> Self {
-        Self {
-            entries: vec![]
-        }
+        Self::new(Self::DEFAULT_SIZE)
     }
 }
 
 impl TranspositionTable {
-    pub fn resize() {
-        todo!();
+    pub const DEFAULT_SIZE: usize = 16;
+    pub const ENTRY_SIZE: usize = size_of::<Transposition>();
+
+    fn index(&self, hash: u64) -> usize {
+        (hash as usize) % self.entries.len()
     }
 
-    pub fn clear() {
-        todo!();
+    pub fn new(size_mib: usize) -> Self {
+        let mut tt = Self {
+            entries: vec![]
+        };
+        tt.resize(size_mib);
+
+        tt
     }
 
-    pub fn get() -> Transposition {
-        todo!();
+    pub fn resize(&mut self, size_mib: usize) {
+        let entry_count = (size_mib << 20) / Self::ENTRY_SIZE;
+        self.entries.resize(entry_count, Transposition::default());
     }
 
-    pub fn insert() {
-        todo!();
+    pub fn clear(&mut self) {
+        self.entries.iter_mut().for_each(|e| *e = Transposition::default());
+    }
+
+    pub fn get(&self, key: u64) -> Transposition {
+        self.entries[self.index(key)]
+    }
+
+    pub fn insert(&mut self, entry: Transposition) {
+        let index = self.index(entry.key);
+        self.entries[index] = entry;
     }
 }
